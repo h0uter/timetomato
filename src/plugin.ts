ba396b93cdb3a877e127f1ca69a3dc07ae20e771
Ellen (E.H.J) Riemens <50137097+ellenriemens@users.noreply.github.com>
2025-05-17 21:30:27 +0200

    try to use global variables

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,13 +2,16 @@ import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
 import { IncrementCounter } from "./actions/increment-counter";
 import { IncrementCounterWouter } from "./actions/increment-counter-wouter";
+import { GlobalIncrementCounter } from "./actions/increment-counter-ellen";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
 
+
 // Register the increment action.
 streamDeck.actions.registerAction(new IncrementCounter());
 streamDeck.actions.registerAction(new IncrementCounterWouter());
+streamDeck.actions.registerAction(new GlobalIncrementCounter());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
